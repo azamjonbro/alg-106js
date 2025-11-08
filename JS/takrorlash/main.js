5f0da0a9f73d1c42f45aac0bdb3363f13963bb32
azamjonbro <azamjondasturchi11@gmail.com>
2025-11-08 17:29:35 +0500

    added

---
+++ JS/takrorlash/main.js
@@ -0,0 +1,15 @@
+console.log(salom);
+
+// let salom = "Hello World";
+// hoistingga uchramaydi va ReferenceError beradi
+
+var salom = "salom dunyo"; // hoistingga uchraydi va undefined beradi
+
+
+let son = 10
+
+console.log(son++); // 10
+console.log(son--); // 11
+console.log(++son); //
+
+
