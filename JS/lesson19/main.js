bca33528adb634c6d4993b48045e0f5dfc9065b6
azamjonbro <azamjondasturchi11@gmail.com>
2025-11-11 18:01:21 +0500

    added

---
+++ JS/lesson19/main.js
@@ -0,0 +1,37 @@
+let obj = {
+    oyatillo: "Aziza",
+    abubakr: "kamola",
+    islomjon:"muslima",
+    teacher:"❤️",
+    ravshan:["magicxon","rayona","iymona"],
+    ibrohimjon: "kamola",
+    MuhammadSoli : "Umida",
+    Islombek : "Charos",
+    Nurmuhammad : "Ruxshona",
+    Elyorbek : "Shukrona",
+    Elbek : "Rayxona",
+    Shuhratbek : "Shahlo",
+    Saidafzalhon: "Laylo",
+    Ravashan : ""
+}
+
+
+
+let param = Object()
+param.name="ali"
+Object.seal(param)
+param.date="21-08-2004"
+// Object.freeze(param)
+param.name="dilshoda"
+
+// let data =Object.entries(param) bu yerda object ichidagi keyni ham valueni ham arrayga o'tkazib qaytarib berdi.
+
+// let data = Object.keys(param) bu yerda esa object ichidagi keyni ozini arrayga o'rab qaytarib berdi. 
+
+// let data = Object.values(param) bu yerda esa value (qiymatlarni) yangi arrayga solib qaytarib berdi.
+
+console.log(data);
+
+console.log(param);
+
+
